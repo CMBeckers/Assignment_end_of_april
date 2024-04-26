2203ec67c3657a6cc378ed7c625f01d67b25093d
Salah Baaziz <salah.baaziz@hotmail.com>
2024-04-26 12:28:02 +0100

    Updated with the plot function, tested on another of my codes and works well

---
+++ Task2 New.py
@@ -0,0 +1,58 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+def spawn(num_people):
+    return np.random.rand(num_people)
+
+def update(opinion,beta,threshold,iterations):
+    opinion_change = []
+    for i in range(iterations):
+        n = np.random.randint(len(opinion))
+        if n == 0:
+            neighbour = n + 1
+        elif n == (len(opinion) - 1):
+            neighbour = n - 1
+        else:
+            neighbour = (n+random.choice([-1,1]))
+        difference = opinion[n] - opinion[neighbour]
+
+        if abs(difference) < threshold:
+            opinion[n] += (beta * (opinion[neighbour] - opinion[n]))
+            opinion[neighbour] += (beta * (opinion[n] - opinion[neighbour])) #most important part so far
+        opinion_change.append(opinion.copy()) #gives you a copy of the same list, not same as deep copy (compound list)
+    return opinion_change
+
+def plot_opinion(opinion_change, iterations, beta, threshold):
+    fig = plt.figure()
+    #first sublot
+    ax1 = fig.add_subplot(1, 2, 1)
+    ax1.hist(opinion_change[-1], bins=10)
+    ax1.set_xlabel('Opinion')
+    ax1.set_ylabel('Number')
+    ax1.set_xticks(np.arange(0, 1.1, 0.2))
+    #second subplot
+    ax2 = fig.add_subplot(1, 2, 2)
+    ax2.plot(range(iterations), opinion_change, 'ro')
+    ax2.set_ylabel('Opinion')
+    ax2.set_xlabel('Iteration')
+    fig.suptitle(f'Coupling: {beta}, Threshold: {threshold}')
+    plt.tight_layout()
+    plt.show()
+
+def defuant_main(beta, threshold):
+    num_people = 2000
+    iterations = 50000
+    opinion_change = update(spawn(num_people), beta, threshold,iterations)
+
+def test_defuant():
+    defuant_main(0.5,0.1)
+    defuant_main(0.5, 0.1)
+    defuant_main(0.5, 0.1)
+    defuant_main(0.5, 0.1) #check threshold values they asked to put in on assignment
+
+def main():
+    #need to put something to do with arg
+
+if __name__ == "__main__":
+    main()
